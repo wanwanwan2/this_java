28be08e015ffd9406c6fc4c2db558edb2e2750a6
WanPatch <121847253+wanwanwan2@users.noreply.github.com>
2023-12-11 09:20:28 +0900

    클래스

---
+++ chapter6/chapter6/src/chapter6/sec07/exam05/Car.java
@@ -0,0 +1,22 @@
+package chapter6.sec07.exam05;
+
+public class Car {
+	String company = "현대자동차";
+	String model;
+	String color;
+	int maxSpeed;
+	
+	Car(String model) {
+		this(model, "은색", 250);
+	}
+	
+	Car(String model, String color) {
+		this(model, color, 250);
+	}
+	
+	Car(String model, String color, int maxSpeed) {
+		this.model = model;
+		this.color = color;
+		this.maxSpeed = maxSpeed;
+	}
+}
---
+++ chapter6/chapter6/src/chapter6/sec08/exam04/CalculatorExample.java
@@ -0,0 +1,19 @@
+package chapter6.sec08.exam04;
+
+public class CalculatorExample {
+
+	public static void main(String[] args) {
+		//객체 생성
+		Calculator myCalu = new Calculator();
+		
+		//정사각형 넓이 구하기
+		double result1 = myCalu.areaRectRange(10);
+		
+		//직사각형 넓이 구하기
+		double result2 = myCalu.areaRectRange(10, 20);
+		
+		System.out.println("정사각형의 넓이 = " + result1);
+		System.out.println("직사각형의 넓이 = " + result2);
+	}
+
+}
