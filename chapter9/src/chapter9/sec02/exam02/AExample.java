5f700a6dfb27ee63de54e623ffca4076817dfee7
WanPatch <121847253+wanwanwan2@users.noreply.github.com>
2023-12-14 19:07:37 +0900

    중복선언

---
+++ chapter9/src/chapter9/sec02/exam02/AExample.java
@@ -0,0 +1,14 @@
+package chapter9.sec02.exam02;
+
+public class AExample {
+
+	public static void main(String[] args) {
+		//A 객체 생성
+		A a =  new A();
+		
+		//A 인스턴스 메소드 호출
+		a.usbB();
+
+	}
+
+}
