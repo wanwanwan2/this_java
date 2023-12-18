8a72046d9b59e49dc454c0641a73ae8a6adfe990
WanPatch <121847253+wanwanwan2@users.noreply.github.com>
2023-12-18 09:54:11 +0900

    예외처리

---
+++ chapter11/chapter11/src/chapter11/sec06/InsufficientException.java
@@ -0,0 +1,9 @@
+package chapter11.sec06;
+
+public class InsufficientException extends Exception {
+	public InsufficientException() {}
+	
+	public InsufficientException(String message) {
+		super(message);
+	} 
+}
